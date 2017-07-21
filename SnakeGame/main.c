d5fd04832f69a23006d928db0be5c5646de65536
buitiendat719 <30335441+buitiendat719@users.noreply.github.com>
2017-07-21 13:50:01 +0700

    Update main.c

--- SnakeGame/main.c
+++ SnakeGame/main.c
@@ -13,3 +13,26 @@ int main()
     printf("Hello world!\n");
     return 0;
 }
+
+void GoUp(int Board[50][50],int SnakeX[400],int SnakeY[400],int *Snakelength, int *FoodX,int *FoodY)
+{
+    int i,j;
+    if(SnakeX[(*Snakelength)-1]==*FoodX&& (SnakeY[(*Snakelength)-1]-1+50)%50==*FoodY)
+    {
+        SnakeX[*Snakelength]= *FoodX;
+        SnakeY[*Snakelength]= *FoodY;
+        (*Snakelength)++;
+
+        NewFood(&(*FoodX),&(*FoodY),SnakeX, SnakeY,*Snakelength);
+
+    }
+    else
+    {
+        for(i= 0; i<*Snakelength-1;i++)
+        {
+            SnakeX[i]=SnakeX[i+1];
+            SnakeY[i]=SnakeY[i+1];
+        }
+        SnakeY[(*Snakelength)-1]= (SnakeY[(*Snakelength)-1]-1+50)%50;
+    }
+}
